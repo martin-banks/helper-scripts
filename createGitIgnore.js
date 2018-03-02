96103236d7caedddf5639c92f15336099f7c5fc0
Martin Banks <anotherbanksy@gmail.com>
2018-03-03 00:06:07 +1100

    style updates

--- createGitIgnore.js
+++ createGitIgnore.js
@@ -7,13 +7,13 @@ const https = require('https')
 
 // Default options
 const options = [
-	'macos',
-	'windows',
-	'node',
-	'adobe',
-	'sublimetext',
-	'visualstudiocode',
-	'vim',
+  'macos',
+  'windows',
+  'node',
+  'adobe',
+  'sublimetext',
+  'visualstudiocode',
+  'vim',
 ].join('%2C')
 
 // Build options into the require url
@@ -26,35 +26,33 @@ const cwd = process.cwd()
 const args = process.argv.slice(2)
 
 const addIgnores = ignores => `
-### Additional custom ignores ###
-${ignores.join('\n')}
-
-
+  ### Additional custom ignores ###
+  ${ignores.join('\n')}
 `
 
 // Make the reques to the api
 https.get(ignoreSource, res => {
-	// Temp variable to store data from stream
-	let output = addIgnores(args)
+  // Temp variable to store data from stream
+  let output = addIgnores(args)
 
-	// tell it what enc type to use so we can do stuff with it
-	res.setEncoding('utf8')
+  // tell it what enc type to use so we can do stuff with it
+  res.setEncoding('utf8')
 
-	// When the data comes in ...
-	res.on('data', d => {
-		// ... write it into the output variable
-		output += d
-	})
-	// When the data stops coming ...
-	res.on('end', () => {
-		// ... get the output object and write to file
-		// using the cwd path captured when the app started
-		fs.writeFile(path.join(cwd, `/.gitignore`), output, err => {
-			if (err) return console.log('OH NOO!!\n', err)
-			return console.log('Ignore file created')
-		})
-	})
-	// Nothing left to do, the 'end' command stops the request function
+  // When the data comes in ...
+  res.on('data', d => {
+    // ... write it into the output variable
+    output += d
+  })
+  // When the data stops coming ...
+  res.on('end', () => {
+    // ... get the output object and write to file
+    // using the cwd path captured when the app started
+    fs.writeFile(path.join(cwd, `/.gitignore`), output, err => {
+      if (err) return console.log('OH NOO!!\n', err)
+      return console.log('Ignore file created')
+    })
+  })
+  // Nothing left to do, the 'end' command stops the request function
 })
-	.on('error', console.log) // just in case
+  .on('error', console.log) // just in case
 
