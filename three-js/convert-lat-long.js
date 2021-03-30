30bddb6af307a2b20b84a04f752f3ba5e03278e2
martin-banks <anotherbanksy@gmail.com>
2021-03-30 16:38:49 +1100

    adds rotation to point to parent center

--- three-js/convert-lat-long.js
+++ three-js/convert-lat-long.js
@@ -1,4 +1,12 @@
 
+const parent = new THREE.Mesh(
+  new THREE.SphereGeometry(10, 32, 32),
+  new THREE.MeshPhongMaterial({ color: '#fff' })
+)
+const child = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshPhongMaterial({ color: 'hotpink' })
+)
 
 // ? In order to use LAT and LONG to plot items on a THREE.js sphere,
 // ? they must first be converted to the appropriate angles represent
@@ -10,13 +18,30 @@ const theta = (lng + 180) * (Math.PI / 180)
 // ? used to plot them in 3d space as a child of a parent model (shpere)
 // ? These coords are relative to the center of the parent
 const x = -1 * Math.sin(phi) * Math.cos(theta)
-const z = Math.sin(phi) * Math.sin(theta)
 const y = Math.cos(phi)
+const z = Math.sin(phi) * Math.sin(theta)
 
 
 // ? These values can then be multiplied by a radius value that will shift them from the center
 // ? So for a child to appear appear on the "surface" of the parent, multiply by the radius of the parent
 // ? increase or decrease this value to render above/below the "surface" respectively
 const surfaceX = x * parent.radius
-const surfaceZ = z * parent.radius
 const surfaceY = y * parent.radius
+const surfaceZ = z * parent.radius
+
+child.position.x = surfaceX
+child.position.y = surfaceZ
+child.position.z = surfaceY
+
+
+// ? What if we want to rotate the child so it points to the center?
+// ? I don't remember what this means!!
+const childVector = new THREE.Vector3(0, 1, 0)
+const childMatrix = new THREE.Matrix4()
+  .makeRotationX(90 * (Math.PI / 180))
+  .makeRotationY(0)
+  .makeRotationZ(theta)
+  .lookAt(child.position, childVector, child.up)
+
+child.quaternion.setFromRotationMatrix(childMatrix)
+
